28d007de8572cbba46f971b2ab329a3a817ee01d
SreehariTS <sreeharisudarshan@gmail.com>
2021-09-26 20:13:13 +0530

    Create DetectIPChange.sh

---
+++ DetectIPChange.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+OLD_IP=""
+NEW_IP=""
+
+read OLD_IP < old_ip.log
+NEW_IP=$(wget -qO- ifconfig.me/ip)
+
+if [ " $NEW_IP " != " $OLD_IP " ];
+then
+        echo "Changed"
+        exit
+fi
+
+echo $NEW_IP > old_ip.log
